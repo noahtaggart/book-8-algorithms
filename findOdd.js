5b886e54d245ed27697547aac6267ae6eb3e90e2
Noah Taggart <noahtag@gmail.com>
2022-05-23 21:12:12 -0500

    two sum

--- findOdd.js
+++ findOdd.js
@@ -10,14 +10,14 @@
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
 export const findOdd = (A) => {
-    let count
+    let count = 0
     for(let i = 0; i<A.length; i++) {
-        for(let j = 0; j<A.length; i++) {
+        for(let j = 0; j<A.length; j++) {
             if (A[i] == A[j]) {
                 count++
             }
-        }
-    } if (count %2 != 0){
+        } if (count % 2 != 0){
         return A[i]
+        }
     }
 }
--- main.js
+++ main.js
@@ -4,14 +4,17 @@ import { buildingHeights } from "./cityPlanner.js";
 import { findOdd } from "./findOdd.js";
 import { highAndLow } from "./highestAndLowest.js";
 import { isPrime } from "./isANumberPrime.js";
+import { moveZeros } from "./movingZerosToTheEnd.js";
 import { binaryArrayToNumber } from "./onesAndZeros.js";
 import { rgb } from "./RGBToHexConversion.js";
 import { solution } from "./SplitStrings.js";
 import { squareDigits } from "./squareEveryDigit.js";
 import { spinWords } from "./StopgninnipSMysdroW.js";
 import { sum } from "./sumOfMultiples.js";
+import { twoSum } from "./twoSum.js";
 import { uniqueInOrder } from "./uniqueInOrder.js";
 
 
-console.log(findOdd(112255555999999))
-
+console.log(findOdd([1,1,1,2,2,2]))
+console.log(moveZeros([ 3, [], '5', null, '5', 5, 8, true, {} ]))
+console.log(twoSum([3, 3], 6))
